dcef7c65f5eb27dbce6b4320fc8b3f89016fc77d
alexandre-mouysset <12258@holbertonstudents.com>
2025-12-09 09:49:39 +0100

    changed commentary

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree
- * @tree: Pointer to the node to measure
+ * binary_tree_size - Measures the total number of nodes in a binary tree
+ * @tree: Pointer to the root node of the tree
  *
- * Return: Depth of the node, or 0 if tree is NULL
+ * Return: Number of nodes in the tree, or 0 if tree is NULL
  *
  * Description:
- *   Measures the number of edges from the given node to the root. Returns 0
- *   if the node is NULL or is the root.
+ *   Recursively counts all nodes in the tree by summing the sizes of the
+ *   left and right subtrees plus one for the current node.
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
